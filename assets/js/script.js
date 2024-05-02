3376e6456069fe6f05e6043eb99099d61d781668
gagan goswami <143880202+gagan-goswami@users.noreply.github.com>
2024-05-02 23:10:06 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+
+$(document).ready(function () {
+    // Initialize Owl Carousel
+    var slider = $('.slider-2').owlCarousel({
+        autoplay: false,
+        loop: true,
+        nav: false,
+        margin: 10,
+        thumbs: true,
+        thumbImage: true,
+        thumbContainerClass: 'owl-thumbs',
+        thumbItemClass: 'owl-thumb-item',
+        dots: false,
+        slideBy: 1,
+        autoplayTimeout: 5000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        responsive: {
+            0: { items: 1 },
+            600: { items: 1 },
+            992: { items: 1 }
+        }
+    });
+
+    // Pause button click event
+    $('.pause-slider').on('click', function () {
+        if ($(this).hasClass('paused')) {
+            $(this).removeClass('paused').html('<i class="far fa-pause-circle"></i>');
+            slider.trigger('play.owl.autoplay');
+        } else {
+            $(this).addClass('paused').html('<i class="far fa-play-circle"></i>');
+            slider.trigger('stop.owl.autoplay');
+        }
+    });
+
+});
